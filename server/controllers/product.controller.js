a86f9e6dd21684b7da86f9a52c304c91761a450f
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-01-26 23:34:08 -0500

    debugged backend

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,4 +1,4 @@
-const Product = require('./models/product.model');
+const Product = require('../models/product.model');
 
 //creates new Product in database
 module.exports.createProduct = (req, res) => {
