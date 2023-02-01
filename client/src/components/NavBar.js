a3ada2bf3259b64216cee60f62be04014438a1c3
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-01-31 21:58:55 -0500

    worked on navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,16 @@ import {Link} from 'react-router-dom';
 
 const NavBar = (props) => {
     return (
-        <Link to ={"/"}><img src={"assets/images/LukjanLogo.png"} alt={"Beans"}/></Link>
+        <div>
+            <nav class={"flex items-end justify-end px-20 py-10 border border-b-black"}>
+                <Link to={"/"} class={"mr-auto"}><img class={"h-12"} src={"assets/images/LukjanLogo.png"} alt={"Beans"}/></Link>
+                <div>
+                    <Link to={"/"}>Products</Link>
+                    <Link to={"/"}>About</Link>
+                    <Link to={"/"}>Contact</Link>
+                </div>
+            </nav>
+        </div>
     )
 }
 export default NavBar;
