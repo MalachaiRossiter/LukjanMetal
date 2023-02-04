6c3f9b65bde681dd748f39f6059ab90f034d2953
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-02-04 00:31:39 -0500

    updated the database model

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
     name: {type: String, required: [true, "please provide a product title"], minLength: [3, "name of product must be at least 3 characters long"]},
+    type: {type: String, required: [true, "please provide a product type"], minLength: [3, "type of product must be at least 3 characters long"]},
     description: {type: String, required: [true, "please provide a product description"], minLength: [3, "description of product must be at least 3 characters long"]},
     imagePath: {type: String, required: [true, "please provide a product image"]},
 }, {timestamps: true});
