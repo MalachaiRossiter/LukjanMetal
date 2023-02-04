1cd1403fa7ee897bea245afa09be4cc378e7707e
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-02-03 23:57:06 -0500

    finished nav bar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,12 @@ const Home = (props) => {
     return (
         <div>
             <div class={"heroSection"}>
-                <h1>Manufacturing high quality products since 1964</h1>
+                <h1><i>Lukjan Metals</i></h1>
+                <h3 class={"heroH3"}>Manufacturing high quality products since 1964</h3>
                 <button class={"btnMajor"}>Learn More &gt;</button>
             </div>
+
+            
         </div>
     )
 }
